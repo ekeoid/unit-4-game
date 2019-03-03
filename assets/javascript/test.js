1e65d0c3ad3e7e1acae34ad877e81e9b8eddae08
Elvin <elveng@gmail.com>
2019-03-02 21:25:00 -0500

    none

---
+++ assets/javascript/test.js
@@ -0,0 +1,15 @@
+function Player () {
+    
+
+    id: "ahsoka",     name: "Ahsoka Tano",            healthPoints: 150, attackPower: 10, counterAttack: 5, position: "-8px   -140px"
+}
+
+class Player {
+    constructor() {
+        this.id = "";
+        this.name = "";
+        this.healthPoints = 0;
+        this.attackPower = 0;
+        this.counterAttackPower = 0;
+    }
+}
