a9d004f61b1b0d757a4251b116c4984ff80e4f62
Elvin <elveng@gmail.com>
2019-03-03 19:56:45 -0500

    modified player values

--- assets/javascript/charlist.js
+++ assets/javascript/charlist.js
@@ -1,16 +1,16 @@
 var characterList = [
-    { id: "ahsoka",     name: "Ahsoka Tano",            healthPoints: 190, attackPower: 10, counterAttack:  7, position: "-8px   -140px" },
-    { id: "anakin",     name: "Anakin Skywalker",       healthPoints: 205, attackPower: 12, counterAttack:  9, position: "-8px   -280px" },
-    { id: "obiwan",     name: "Obi-Wan Kenobi",         healthPoints: 210, attackPower: 16, counterAttack:  8, position: "-8px   -400px" },
+    { id: "ahsoka",     name: "Ahsoka Tano",            healthPoints: 190, attackPower: 10, counterAttack: 10, position: "-8px   -140px" },
+    { id: "anakin",     name: "Anakin Skywalker",       healthPoints: 205, attackPower: 12, counterAttack: 15, position: "-8px   -280px" },
+    { id: "obiwan",     name: "Obi-Wan Kenobi",         healthPoints: 210, attackPower: 16, counterAttack: 12, position: "-8px   -400px" },
     { id: "yoda",       name: "Yoda",                   healthPoints: 220, attackPower: 11, counterAttack: 15, position: "-8px   -560px" },
-    { id: "r2d2",       name: "R2-D2",                  healthPoints: 150, attackPower:  5, counterAttack:  5, position: "-8px   -690px" },
-    { id: "rex",        name: "Captain Rex",            healthPoints: 250, attackPower: 12, counterAttack:  7, position: "-8px   -830px" },
-    { id: "satine",     name: "Duchess Satine Kryze",   healthPoints: 260, attackPower:  5, counterAttack:  5, position: "-8px   -950px" },
-    { id: "darthmaul",  name: "Darth Maul",             healthPoints: 280, attackPower: 14, counterAttack: 12, position: "-445px -145px" },
-    { id: "savage",     name: "Savage Opress",          healthPoints: 180, attackPower:  9, counterAttack:  6, position: "-450px -275px" },
-    { id: "palpatine",  name: "Chancellor Palpatine",   healthPoints: 350, attackPower: 15, counterAttack: 14, position: "-445px -410px" },
-    { id: "asajj",      name: "Asajj Ventress",         healthPoints: 150, attackPower: 10, counterAttack:  9, position: "-440px -540px" },
-    { id: "previzla",   name: "Pre Vizsla",             healthPoints: 150, attackPower:  8, counterAttack:  6, position: "-445px -690px" },
+    { id: "r2d2",       name: "R2-D2",                  healthPoints: 150, attackPower:  5, counterAttack: 10, position: "-8px   -690px" },
+    { id: "rex",        name: "Captain Rex",            healthPoints: 250, attackPower: 12, counterAttack: 17, position: "-8px   -830px" },
+    { id: "satine",     name: "Duchess Satine Kryze",   healthPoints: 260, attackPower:  5, counterAttack: 10, position: "-8px   -950px" },
+    { id: "darthmaul",  name: "Darth Maul",             healthPoints: 280, attackPower: 14, counterAttack: 22, position: "-445px -145px" },
+    { id: "savage",     name: "Savage Opress",          healthPoints: 180, attackPower:  9, counterAttack: 16, position: "-450px -275px" },
+    { id: "palpatine",  name: "Chancellor Palpatine",   healthPoints: 350, attackPower: 15, counterAttack: 24, position: "-445px -410px" },
+    { id: "asajj",      name: "Asajj Ventress",         healthPoints: 150, attackPower: 10, counterAttack: 20, position: "-440px -540px" },
+    { id: "previzla",   name: "Pre Vizsla",             healthPoints: 150, attackPower:  8, counterAttack: 16, position: "-445px -690px" },
     { id: "grevious",   name: "Grevious",               healthPoints: 300, attackPower: 10, counterAttack: 12, position: "-445px -830px" },
-    { id: "wilhuff",    name: "Wilhuff Tarkin",         healthPoints: 180, attackPower: 12, counterAttack:  8, position: "-445px -955px" }
+    { id: "wilhuff",    name: "Wilhuff Tarkin",         healthPoints: 180, attackPower: 12, counterAttack: 18, position: "-445px -955px" }
 ];
